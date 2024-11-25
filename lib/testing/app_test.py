dd82b8f03d31b70ed5481c7b0c20825cbd7c494c
hamdiabdiaden <diagodu198@gmail.com>
2024-11-25 15:30:22 +0300

    first commit

--- lib/testing/app_test.py
+++ lib/testing/app_test.py
@@ -23,8 +23,9 @@ class TestAppPy:
 
     def test_prints_hello_world(self):
         '''
-        prints "Hello World! Pass this test, please."
+      print( prints "Hello World! Pass this test, please.")
         '''
+    
         captured_out = io.StringIO()
         sys.stdout = captured_out
         runpy.run_path("lib/app.py")
